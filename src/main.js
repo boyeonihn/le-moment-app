e13481a2f8e9af412c2bf3efcbe2d6e132a398a0
Boyeon Ihn <boyeonihn@gmail.com>
2023-04-06 17:54:38 +0900

    feat: add greeting to user based on current time

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import './style.css';
 
-const loggedIn = false;
+const loggedIn = true;
 const loggedInContainer = document.querySelector('.loggedIn');
+const notLoggedInContainer = document.querySelector('.notLoggedIn');
+const username = 'Bonnie';
+
 const clock = document.getElementById('clock');
+const greetingBox = document.getElementById('greeting');
 
 if (loggedIn) {
   loggedInContainer.style.display = 'block';
@@ -11,6 +15,22 @@ if (loggedIn) {
   loggedinStartup();
 } else {
   loggedInContainer.style.display = 'none';
+  notLoggedInContainer.style.display = 'block';
+}
+
+function loggedinStartup() {
+  const currentHour = updateClock();
+  let greetingType;
+  if (currentHour >= 0 && currentHour <= 11) {
+    greetingType = 'Good morning';
+  } else if (currentHour >= 12 && currentHour <= 16) {
+    greetingType = 'Good afternoon';
+  } else if ((currentHour >= 15 && currentHour <= 24) || currentHour == 0) {
+    greetingType = 'Good evening';
+  }
+  greetingBox.innerText = `${greetingType}, ${username}.`;
+}
+
 function updateClock() {
   let now = new Date();
   let time = `${now.getHours()}:${now.getMinutes()}`;
