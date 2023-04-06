58bda1eb936d77a6feceb90851988c7006204a8b
Boyeon Ihn <boyeonihn@gmail.com>
2023-04-06 17:52:20 +0900

    feat: add current time feature

--- src/main.js
+++ src/main.js
@@ -2,9 +2,20 @@ import './style.css';
 
 const loggedIn = false;
 const loggedInContainer = document.querySelector('.loggedIn');
+const clock = document.getElementById('clock');
 
 if (loggedIn) {
   loggedInContainer.style.display = 'block';
+  notLoggedInContainer.style.display = 'none';
+  updateClock();
+  loggedinStartup();
 } else {
   loggedInContainer.style.display = 'none';
+function updateClock() {
+  let now = new Date();
+  let time = `${now.getHours()}:${now.getMinutes()}`;
+  clock.innerText = time;
+
+  setTimeout(updateClock, 1000);
+  return now.getHours();
 }
