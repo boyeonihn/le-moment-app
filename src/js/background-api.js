e02470b7b48cadbd7c7d9a57d3d8c377470d3284
Boyeon Ihn <boyeonihn@gmail.com>
2023-04-16 12:13:09 +0900

    fix: add backup bgImg for failed fetch request

--- src/js/background-api.js
+++ src/js/background-api.js
@@ -1,14 +1,29 @@
 export async function getBackgroundImage() {
-  const response = await fetch(
-    'https://api.unsplash.com/photos/random?client_id=O2i326sq5rbgynSMb0RNvC4EbApEpopFiPYeyPJzWx4&query=mountains&orientation=landscape&count=1'
-  )
+  const response = await fetch('url')
+    // 'https://api.unsplash.com/photos/random?client_id=O2i326sq5rbgynSMb0RNvC4EbApEpopFiPYeyPJzWx4&query=blue-sky&orientation=landscape&count=1'
     .then((res) => res.json())
     .then((data) => {
       const imageUrl = data[0].urls.full;
       const imageAuthor = data[0].user.name;
-      console.log(imageUrl);
-      console.log(`hello background`);
 
-      document.body.style.backgroundImage = `url(${imageUrl})`;
+      let bgImg = new Image();
+      bgImg.onload = function () {
+        document.body.style.backgroundImage = `url(${bgImg.src})`;
+        document.getElementById(
+          'photoCredit'
+        ).innerText = `Photo credit:${imageAuthor}`;
+      };
+      bgImg.src = imageUrl;
+    })
+    .catch((error) => {
+      console.log(error);
+      let bgImg = new Image();
+      bgImg.onload = function () {
+        document.body.style.backgroundImage = `url(${bgImg.src})`;
+        document.getElementById(
+          'photoCredit'
+        ).innerText = `Photo Credit: Vincentiu Solomon`;
+      };
+      bgImg.src = 'public/defaultBackground.jpg';
     });
 }
