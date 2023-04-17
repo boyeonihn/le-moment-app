f16f88f0188563b4c263ea771fc995e5c7344751
Boyeon Ihn <boyeonihn@gmail.com>
2023-04-17 12:50:21 +0900

    fix: change backgroundImg directory

--- src/js/backgroundImg.js
+++ src/js/backgroundImg.js
@@ -24,6 +24,6 @@ export async function getBackgroundImage() {
           'photoCredit'
         ).innerText = `Photo: Vincentiu Solomon`;
       };
-      bgImg.src = 'public/defaultBackground.jpg';
+      bgImg.src = '/defaultBackground.jpg';
     });
 }
